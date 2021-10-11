7119e39f64213619d911128a227f7c31ee753905
Mike <36593824+mikevansighem@users.noreply.github.com>
2021-10-11 19:09:54 +0200

    Update config.h

--- opentherm-ha/config.h
+++ opentherm-ha/config.h
@@ -18,8 +18,8 @@ const char* mqtt_password = "xxxxxx";
 const int mqtt_port = 1883;
 
 // Master OpenTherm Shield pins configuration
-const int OT_IN_PIN = 4;  //for Arduino, 4 for ESP8266 (D2), 21 for ESP32
-const int OT_OUT_PIN = 5; //for Arduino, 5 for ESP8266 (D1), 22 for ESP32
+const int OT_IN_PIN = D2;  //for Arduino, 4 for ESP8266 (D2), 21 for ESP32
+const int OT_OUT_PIN = D1; //for Arduino, 5 for ESP8266 (D1), 22 for ESP32
 
 // Temperature sensor pin
 const int ROOM_TEMP_SENSOR_PIN = 14; //for Arduino, 14 for ESP8266 (D5), 18 for ESP32
